a762a0f38fd9fe1ff8a7829283e4ecdec5d3d43b
Chance093 <chance.dev093@gmail.com>
2023-10-09 21:41:15 -0700

    Finish setting up drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <main>Home</main>;
+import { serverClient } from '@/app/_trpc/server';
+import TodoList from './_components/TodoList';
+
+export default async function Home() {
+  const todos = await serverClient.getTodos();
+
+  return (
+    <main>
+      <div>
+        <TodoList initTodos={todos} />
+      </div>
+    </main>
+  );
 }
