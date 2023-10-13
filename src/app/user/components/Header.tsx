dddeab68908f0d510b8716c299f7d2370f627576
Chance Espinosa <chance.dev093@gmail.com>
2023-10-13 02:42:36 -0700

    Add basic styling to Header component

--- src/app/user/components/Header.tsx
+++ src/app/user/components/Header.tsx
@@ -2,7 +2,7 @@ import { UserButton } from '@clerk/nextjs';
 
 export default function Header() {
   return (
-    <header>
+    <header className='h-16 flex justify-between items-center bg-gradientEnd px-5'>
       <div>hamburger</div>
       <UserButton afterSignOutUrl='/' />
     </header>
