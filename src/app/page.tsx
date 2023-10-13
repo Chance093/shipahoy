ad240b8de420a49f54cebffffc6543bfa2a3a851
Chance Espinosa <chance.dev093@gmail.com>
2023-10-12 19:20:25 -0700

    Use prettier on app router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { getUserAuth } from '@/lib/auth/utils';
 import { UserButton } from '@clerk/nextjs';
 
 export default async function Home() {
-	const userAuth = await getUserAuth();
-	return (
-		<main className=''>
-			<UserButton afterSignOutUrl='/' />
-			<pre>{JSON.stringify(userAuth, null, 2)}</pre>
-		</main>
-	);
+  const userAuth = await getUserAuth();
+  return (
+    <main className=''>
+      <UserButton afterSignOutUrl='/' />
+      <pre>{JSON.stringify(userAuth, null, 2)}</pre>
+    </main>
+  );
 }
