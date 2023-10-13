26e1b9d42164135790b2426e63684b1fb6596009
Chance Espinosa <chance.dev093@gmail.com>
2023-10-13 01:23:30 -0700

    Create user layout

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -2,9 +2,9 @@ import { UserButton } from '@clerk/nextjs';
 
 export default function Dashboard() {
   return (
-    <main>
+    <>
       <div>Dashboard</div>
       <UserButton afterSignOutUrl='/' />
-    </main>
+    </>
   );
 }
