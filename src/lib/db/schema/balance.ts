9b2d0543c520fd9b0f58dc85671a0496b7a82fcd
Chance093 <chance.dev093@gmail.com>
2023-10-17 17:00:58 -0700

    Add indexes to schema

--- src/lib/db/schema/balance.ts
+++ src/lib/db/schema/balance.ts
@@ -4,16 +4,21 @@ import {
   varchar,
   decimal,
   timestamp,
+  uniqueIndex,
 } from 'drizzle-orm/mysql-core';
 import { relations } from 'drizzle-orm';
 import { invoice } from './invoice';
 
-export const balance = mysqlTable('balance', {
-  id: serial('id').primaryKey(),
-  userId: varchar('user_id', { length: 200 }).notNull(),
-  amount: decimal('amount', { precision: 7, scale: 2 }),
-  updatedAt: timestamp('updated_at').defaultNow(),
-});
+export const balance = mysqlTable(
+  'balance',
+  {
+    id: serial('id').primaryKey(),
+    userId: varchar('user_id', { length: 200 }).notNull(),
+    amount: decimal('amount', { precision: 7, scale: 2 }),
+    updatedAt: timestamp('updated_at').defaultNow(),
+  },
+  (table) => ({ userIdx: uniqueIndex('user_idx').on(table.userId) })
+);
 
 export const balanceRelations = relations(balance, ({ many }) => ({
   invoice: many(invoice),
