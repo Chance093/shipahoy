169e4e8b4adc6190615e5f0e20c8b73c2688783a
Chance Espinosa <chance.dev093@gmail.com>
2023-10-19 18:18:17 -0700

    Update SignInButtons to redirect to user/dashboard

--- src/app/components/SignInButtons.tsx
+++ src/app/components/SignInButtons.tsx
@@ -1,14 +1,28 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useAuth } from '@clerk/clerk-react';
 
 export default function SignInButtons() {
   const router = useRouter();
+  const { isSignedIn } = useAuth();
 
   return (
     <div className='flex gap-5'>
-      <button onClick={() => router.push('/sign-in')}>Log In</button>
-      <button onClick={() => router.push('/sign-up')}>Sign Up</button>
+      <button
+        onClick={() =>
+          isSignedIn ? router.push('/user/dashboard') : router.push('sign-in')
+        }
+      >
+        Log In
+      </button>
+      <button
+        onClick={() =>
+          isSignedIn ? router.push('/user/dashboard') : router.push('sign-up')
+        }
+      >
+        Sign Up
+      </button>
     </div>
   );
 }
