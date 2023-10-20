80de9a6064b0a7c362b3efd7b46a59e0574dc1e7
Chance Espinosa <chance.dev093@gmail.com>
2023-10-20 03:28:45 -0700

    Update header position to fixed

--- src/app/user/components/Header.tsx
+++ src/app/user/components/Header.tsx
@@ -2,8 +2,8 @@ import { UserButton } from '@clerk/nextjs';
 
 export default function Header() {
   return (
-    <header className='h-16 flex justify-between items-center bg-heading/5 border-b border-purple-200/20 px-5'>
-      <div>hamburger</div>
+    <header className='h-16 w-full flex justify-between fixed items-center bg-gradient-start border-b border-purple-200/20 px-5'>
+      <div>E</div>
       <UserButton afterSignOutUrl='/' />
     </header>
   );
