537fa1a145292630e1556d90fd8789e97f28137b
Chance Espinosa <chance.dev093@gmail.com>
2023-10-20 13:51:09 -0700

    Update dashboard styling

--- src/app/user/components/Header.tsx
+++ src/app/user/components/Header.tsx
@@ -1,10 +1,13 @@
+'use client';
 import { UserButton } from '@clerk/nextjs';
+import Sidebar from './Sidebar';
 
 export default function Header() {
   return (
-    <header className='h-16 w-full flex justify-between fixed items-center bg-gradient-start border-b border-purple-200/20 px-5'>
+    <header className='h-16 w-full flex justify-between fixed top-0 items-center bg-gradient-start border-b border-purple-200/20 px-5'>
       <div>E</div>
       <UserButton afterSignOutUrl='/' />
+      <Sidebar />
     </header>
   );
 }
--- src/app/user/components/Sidebar.tsx
+++ src/app/user/components/Sidebar.tsx
@@ -1,3 +1,13 @@
 export default function Sidebar() {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside className='h-screen w-72 bg-gradient-start fixed top-0 left-0 border-r border-purple-200/20'>
+      <nav>
+        <ul>
+          <li>Dashboard</li>
+          <li>Create Label</li>
+          <li>Accounting</li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
