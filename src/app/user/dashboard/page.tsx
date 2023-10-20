0f18c1087c80a9a1db11ea45214bc72fbb73c6af
Chance Espinosa <chance.dev093@gmail.com>
2023-10-19 17:58:10 -0700

    Update label count section

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -1,27 +1,33 @@
 import { serverClient } from '@/lib/trpc/server';
+import {format} from 'date-fns';
+
 
 export default async function Dashboard() {
   const balance = await serverClient.balance.getBalance();
   const shippingHistory = await serverClient.labelGroup.getShippingHistory();
+
+  let totalLabels = 0;
+  shippingHistory?.map( item => totalLabels += item.label_group.labelCount)
+
   return (
     <main className='flex flex-col gap-6 px-5 py-7 '>
-      <h1 className='heading'>Welcome Back!</h1>
+      <h1 className='heading pl-2'>Welcome Back!</h1>
       <section className='h-32 flex flex-col justify-between card p-5'>
         <p className='font-bold'>Labels</p>
-        <p className='text-4xl'>25</p>
+        <p className='text-4xl'>{totalLabels}</p>
       </section>
       <section className='h-32 flex flex-col justify-between card p-5'>
         <p className='font-bold'>Balance</p>
         <p className='text-4xl'>$ {balance ? balance[0].amount : '0.00'}</p>
       </section>
       <section className='flex flex-col gap-6'>
-        <h2 className='subheading'>Shipping History</h2>
+        <h2 className='subheading pl-2'>Shipping History</h2>
 
         <div className='w-full card overflow-x-scroll'>
           <table className='border-collapse text-left overflow-x-scroll'>
             <thead>
               <tr className='border-b border-purple-200/20'>
-                <th className='p-5'>Invoice#</th>
+                <th className='p-5'>#</th>
                 <th className='p-5'>Date</th>
                 <th className='p-5'>Size</th>
                 <th className='p-5'>Type</th>
@@ -37,8 +43,8 @@ export default async function Dashboard() {
                     className='border-b border-purple-200/20 text-xs whitespace-nowrap'
                     key={item.label_group.id}
                   >
-                    <td className='p-5'>{item.label_group.id}</td>
-                    <td className='p-5'>{item.invoice.createAt?.toString()}</td>
+                    <td className='p-5'>{item.invoice.id}</td>
+                    <td className='p-5'>{format(item.invoice.createAt!, 'MM-dd-yyyy')}</td>
                     <td className='p-5'>{item.label_group.labelCount}</td>
                     <td className='p-5'>{item.usps_service.service}</td>
                     <td className='p-5'>${item.invoice.totalPrice}</td>
