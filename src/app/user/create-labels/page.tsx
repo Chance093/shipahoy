dddc99c7b7cead1aab472a678f670797ec253610
Chance Espinosa <chance.dev093@gmail.com>
2023-10-20 14:19:12 -0700

    Fix label and accounting page margin

--- src/app/user/create-labels/page.tsx
+++ src/app/user/create-labels/page.tsx
@@ -3,11 +3,10 @@
 import HandleCsv from './components/HandleCsv';
 import SingleLabelCreation from './components/SingleLabelCreation';
 export default function placeholder() {
-
-    return (
-        <main className="">
-            <HandleCsv />
-            <SingleLabelCreation />
-        </main>
-    )
+  return (
+    <main className='mt-16 ml-72'>
+      <HandleCsv />
+      <SingleLabelCreation />
+    </main>
+  );
 }
