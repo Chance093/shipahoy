bd70d396ab83e8a7ea5b9257c2e38e70cdfa086d
Chance Espinosa <chance.dev093@gmail.com>
2023-10-20 14:02:22 -0700

    Update styling and links on sidebar

--- src/app/user/components/Sidebar.tsx
+++ src/app/user/components/Sidebar.tsx
@@ -1,11 +1,19 @@
+import Link from 'next/link';
+
 export default function Sidebar() {
   return (
     <aside className='h-screen w-72 bg-gradient-start fixed top-0 left-0 border-r border-purple-200/20'>
-      <nav>
-        <ul>
-          <li>Dashboard</li>
-          <li>Create Label</li>
-          <li>Accounting</li>
+      <nav className='m-12 mt-32 text-xl'>
+        <ul className='flex flex-col gap-5'>
+          <li className='cursor-pointer hover:text-purple-800'>
+            <Link href='/user/dashboard'>Dashboard</Link>
+          </li>
+          <li className='cursor-pointer hover:text-purple-800'>
+            <Link href='/user/create-labels'>Create Label</Link>
+          </li>
+          <li className='cursor-pointer hover:text-purple-800'>
+            <Link href='/user/accounting'>Accounting</Link>
+          </li>
         </ul>
       </nav>
     </aside>
