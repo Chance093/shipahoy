bf61d71653f8bc5b2c6f20b064e961ca97334a99
Chance093 <chance.dev093@gmail.com>
2023-11-04 03:17:06 -0700

    Create balance update mutation

--- src/server/api/routers/balance.ts
+++ src/server/api/routers/balance.ts
@@ -1,6 +1,7 @@
-import { eq } from 'drizzle-orm';
-import { protectedProcedure, createTRPCRouter } from '../trpc';
-import { balance } from '~/server/db/schema';
+import { eq } from "drizzle-orm";
+import { protectedProcedure, createTRPCRouter } from "../trpc";
+import { balance } from "~/server/db/schema";
+import { z } from "zod";
 
 export const balanceRouter = createTRPCRouter({
   getAmount: protectedProcedure.query(({ ctx }) => {
@@ -9,4 +10,30 @@ export const balanceRouter = createTRPCRouter({
       columns: { amount: true },
     });
   }),
+
+  add: protectedProcedure.mutation(async ({ ctx }) => {
+    const existingBalance = await ctx.db.query.balance.findFirst({
+      where: eq(balance.userId, ctx.auth.userId),
+    });
+    if (existingBalance) return;
+    await ctx.db.insert(balance).values({
+      userId: ctx.auth.userId,
+      amount: "0.00",
+    });
+  }),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        amount: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db
+        .update(balance)
+        .set({
+          amount: input.amount,
+        })
+        .where(eq(balance.userId, ctx.auth.userId));
+    }),
 });
