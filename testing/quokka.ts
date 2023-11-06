d6ba334513a311ad30e89076a6266668fef3e832
proglokan <92769441+proglokan@users.noreply.github.com>
2023-11-06 15:05:25 -0800

    Start writing res logic

--- testing/quokka.ts
+++ testing/quokka.ts
@@ -1,1 +1,167 @@
 'use strict';
+const res = {
+	"type": "success",
+	"message": "Bulk order created successfully",
+	"bulkOrder": {
+		"_id": "65a0e1f5b1b30e8a0d7f4e0",
+		"uuid": "b1c50a23-965b-4f51-81a3-7a28810187d1",
+		"orders": [
+			{
+				"_id": "65a0e1f5b1b30e8a0d7f4df",
+				"labelType": {
+					"_id": "65a0e1f5b1b30e8a0d7f4de",
+					"name": "USPS Priority",
+					"uid": "priority"
+				},
+				"fromName": "Majd Bigham",
+				"fromStreet": "6609 Steen St",
+				"fromStreet2": "",
+				"fromCity": "Canal Winchester",
+				"fromZip": "43110",
+				"fromPhone": "",
+				"toName": "Dino DeRoia",
+				"toStreet": "7965 Princewood Dr",
+				"toStreet2": "",
+				"toCity": "Hudson",
+				"toState": "OH",
+				"toZip": "44236-1589",
+				"toPhone": "",
+				"weight": "2",
+				"price": 1,
+				"tracking": "9206234256349763495",
+				"pdf": "./labels/65a0e1f5b1b30e8a0d7f4f/92055901755477321149763496.pdf",
+				"OrderId": "65a0e1f5b1b30e8a0d7f4dd",
+				"uuid": "65a0e1f5b1b30e8a0d7f4dd",
+				"user": "65a0e1f5b1b30e8a0d7f4dc",
+				"createdAt": "2023-09-22T11:42:34.829Z",
+				"updatedAt": "2023-09-22T11:42:34.829Z",
+				"__v": 0
+			},
+			{
+				"_id": "65a0e1f5b1b30e8a0d7f4df",
+				"labelType": {
+					"_id": "65a0e1f5b1b30e8a0d7f4de",
+					"name": "USPS Priority",
+					"uid": "priority"
+				},
+				"fromName": "Majd Bigham",
+				"fromStreet": "6609 Steen St",
+				"fromStreet2": "",
+				"fromCity": "Canal Winchester",
+				"fromZip": "43110",
+				"fromPhone": "",
+				"toName": "Dino DeRoia",
+				"toStreet": "7965 Princewood Dr",
+				"toStreet2": "",
+				"toCity": "Hudson",
+				"toState": "OH",
+				"toZip": "44236-1589",
+				"toPhone": "",
+				"weight": "2",
+				"price": 1,
+				"tracking": "9206234256349763495",
+				"pdf": "./labels/65a0e1f5b1b30e8a0d7f4f/92055901755477321149763496.pdf",
+				"OrderId": "65a0e1f5b1b30e8a0d7f4dd",
+				"uuid": "65a0e1f5b1b30e8a0d7f4dd",
+				"user": "65a0e1f5b1b30e8a0d7f4dc",
+				"createdAt": "2023-09-22T11:42:34.829Z",
+				"updatedAt": "2023-09-22T11:42:34.829Z",
+				"__v": 0
+			},
+			{
+				"_id": "65a0e1f5b1b30e8a0d7f4df",
+				"labelType": {
+					"_id": "65a0e1f5b1b30e8a0d7f4de",
+					"name": "USPS Priority",
+					"uid": "priority"
+				},
+				"fromName": "Majd Bigham",
+				"fromStreet": "6609 Steen St",
+				"fromStreet2": "",
+				"fromCity": "Canal Winchester",
+				"fromZip": "43110",
+				"fromPhone": "",
+				"toName": "Dino DeRoia",
+				"toStreet": "7965 Princewood Dr",
+				"toStreet2": "",
+				"toCity": "Hudson",
+				"toState": "OH",
+				"toZip": "44236-1589",
+				"toPhone": "",
+				"weight": "2",
+				"price": 1,
+				"tracking": "9206234256349763495",
+				"pdf": "./labels/65a0e1f5b1b30e8a0d7f4f/92055901755477321149763496.pdf",
+				"OrderId": "65a0e1f5b1b30e8a0d7f4dd",
+				"uuid": "65a0e1f5b1b30e8a0d7f4dd",
+				"user": "65a0e1f5b1b30e8a0d7f4dc",
+				"createdAt": "2023-09-22T11:42:34.829Z",
+				"updatedAt": "2023-09-22T11:42:34.829Z",
+				"__v": 0
+			},
+			{
+				"_id": "65a0e1f5b1b30e8a0d7f4df",
+				"labelType": {
+					"_id": "65a0e1f5b1b30e8a0d7f4de",
+					"name": "USPS Priority",
+					"uid": "priority"
+				},
+				"fromName": "Majd Bigham",
+				"fromStreet": "6609 Steen St",
+				"fromStreet2": "",
+				"fromCity": "Canal Winchester",
+				"fromZip": "43110",
+				"fromPhone": "",
+				"toName": "Dino DeRoia",
+				"toStreet": "7965 Princewood Dr",
+				"toStreet2": "",
+				"toCity": "Hudson",
+				"toState": "OH",
+				"toZip": "44236-1589",
+				"toPhone": "",
+				"weight": "2",
+				"price": 1,
+				"tracking": "9206234256349763495",
+				"pdf": "./labels/65a0e1f5b1b30e8a0d7f4f/92055901755477321149763496.pdf",
+				"OrderId": "65a0e1f5b1b30e8a0d7f4dd",
+				"uuid": "65a0e1f5b1b30e8a0d7f4dd",
+				"user": "65a0e1f5b1b30e8a0d7f4dc",
+				"createdAt": "2023-09-22T11:42:34.829Z",
+				"updatedAt": "2023-09-22T11:42:34.829Z",
+				"__v": 0
+			}
+		],
+		"user": "65a0e1f5b1b30e8a0d7f4dc",
+		"total": 1,
+		"zip": "./labels/65a0e1f5b1b30e8a0d7f4e0/labels.zip",
+		"pdf": "./labels/65a0e1f5b1b30e8a0d7f4dc/labels.pdf",
+		"csv": "./labels/65a0e1f5b1b30e8a0d7f4dc/labels.csv",
+		"createdAt": "2023-09-25T15:00:00.000Z",
+		"updatedAt": "2023-09-25T15:00:00.000Z",
+		"__v": 0
+	},
+	"orderStatus": []
+}
+
+const { orders } = res.bulkOrder;
+for (const order of orders) {
+	const {
+		fromName,
+		fromStreet,
+		fromStreet2,
+		fromCity,
+		fromZip,
+		fromPhone,
+		toName,
+		toStreet,
+		toStreet2,
+		toCity,
+		toState,
+		toZip,
+		toPhone,
+		weight,
+		price,
+		tracking
+	} = order;
+	console.log(fromName, fromStreet, fromStreet2, fromCity, fromZip, fromPhone, toName, toStreet, toStreet2, toCity, toState, toZip, toPhone, weight, price, tracking);
+}
