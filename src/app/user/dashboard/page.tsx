ffa4a2522185057826b3b31ff3aff9f5a01bc8e4
Chance093 <chance.dev093@gmail.com>
2023-11-06 16:57:33 -0800

    Fix card borders

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -11,13 +11,17 @@ export default async function Dashboard() {
     <main className="ml-72 flex flex-col gap-8 px-10 py-10">
       <h1 className="pl-2 text-4xl">Dashboard</h1>
       <section className="flex gap-8">
-        <section className="bg-radial-gradient before:bg-linear-gradient relative flex  h-48 max-w-xs flex-1 flex-col justify-between rounded-2xl  p-5 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-[194px] before:w-[322px] before:-translate-x-[1px] before:-translate-y-[1px] before:rounded-2xl before:content-['']">
-          <p>Labels</p>
-          <p className="text-4xl">{labelCount}</p>
+        <section className="bg-linear-gradient h-48 w-80 rounded-2xl">
+          <div className="bg-radial-gradient flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col justify-between rounded-2xl  p-5">
+            <p>Labels</p>
+            <p className="text-4xl">{labelCount}</p>
+          </div>
         </section>
-        <section className="bg-radial-gradient before:bg-linear-gradient relative flex  h-48 max-w-xs flex-1 flex-col justify-between rounded-2xl  p-5 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-[194px] before:w-[322px] before:-translate-x-[1px] before:-translate-y-[1px] before:rounded-2xl before:content-['']">
-          <p>Balance</p>
-          <p className="text-4xl">$ {balance ? balance.amount : "0.00"}</p>
+        <section className="bg-linear-gradient h-48 w-80 rounded-2xl">
+          <div className="bg-radial-gradient flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col justify-between rounded-2xl  p-5">
+            <p>Balance</p>
+            <p className="text-4xl">$ {balance ? balance.amount : "0.00"}</p>
+          </div>
         </section>
       </section>
 
