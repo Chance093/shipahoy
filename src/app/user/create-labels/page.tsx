607189a175d9e07eb3f3d4ec0852b5b4168b7b44
Chance093 <chance.dev093@gmail.com>
2023-11-06 17:51:05 -0800

    Update createLabels main page

--- src/app/user/create-labels/page.tsx
+++ src/app/user/create-labels/page.tsx
@@ -1,10 +1,11 @@
-'use strict';
+"use strict";
 // import TestLabelsApiReq from './components/ApiReq';
-import HandleCsv from './components/HandleCsv';
-import SingleLabelCreation from './components/SingleLabelCreation';
+import HandleCsv from "./components/HandleCsv";
+import SingleLabelCreation from "./components/SingleLabelCreation";
 export default function placeholder() {
   return (
-    <main className='mt-16 ml-72'>
+    <main className="ml-72 flex flex-col gap-8 px-10 py-10">
+      <h1 className="pl-2 text-4xl">Create Labels</h1>
       <HandleCsv />
       <SingleLabelCreation />
     </main>
