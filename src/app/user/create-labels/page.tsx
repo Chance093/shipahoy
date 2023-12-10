903b5ac6a3b687fe8243c48ff3896d8a06b1fa5c
Chance093 <chance.dev093@gmail.com>
2023-12-09 16:39:21 -0800

    Create page divider

--- src/app/user/create-labels/page.tsx
+++ src/app/user/create-labels/page.tsx
@@ -1,19 +1,19 @@
 "use strict";
 // import TestLabelsApiReq from './components/ApiReq';
 
-import HandleCsv from './components/HandleCsv';
-import SingleLabelCreation from './components/SingleLabelCreation';
-import Modal from '~/app/components/Modal';
+import HandleCsv from "./components/HandleCsv";
+import SingleLabelCreation from "./components/SingleLabelCreation";
+import Modal from "~/app/components/Modal";
 
 export default function placeholder() {
   async function onClose() {
-    'use server';
-    console.log('onClose');
+    "use server";
+    console.log("onClose");
   }
 
   async function onOk() {
-    'use server';
-    console.log('onOk');
+    "use server";
+    console.log("onOk");
   }
 
   return (
@@ -21,6 +21,11 @@ export default function placeholder() {
       <h1 className="pl-2 text-4xl">Create Labels</h1>
 
       <HandleCsv />
+      <section className="my-8 flex items-center gap-8">
+        <div className="flex-1 border-b border-[#b4a3d8]"></div>
+        <div className="h-2 w-2 rounded-full bg-[#b4a3d8]"></div>
+        <div className="flex-1 border-b border-[#b4a3d8]"></div>
+      </section>
       <SingleLabelCreation />
     </main>
   );
