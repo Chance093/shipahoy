a87f4183f831ea9d827f6250d100e7ce2aee4e6d
Chance093 <chance.dev093@gmail.com>
2023-12-11 15:01:05 -0800

    Finish v1 styling of handleCSV section

--- src/app/user/create-labels/components/HandleCsv.tsx
+++ src/app/user/create-labels/components/HandleCsv.tsx
@@ -173,28 +173,51 @@ export default function HandleCsv() {
   return (
     <>
       <section className="rounded-2xl bg-linear-gradient">
-        <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] translate-x-[1.5px] translate-y-[1.5px] flex-col justify-between rounded-2xl bg-radial-gradient p-5">
-          <h2 className="p-2 text-center text-2xl">Upload CSV</h2>
-          <div>
-            <p>Service: USPS Priority 0-70lbs</p>
-            <p>Label: e-VS</p>
+        <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] translate-x-[1.5px] translate-y-[1.5px] flex-col gap-8 rounded-2xl bg-radial-gradient p-5">
+          <h2 className="text-center text-2xl">Upload CSV</h2>
+          <div className="flex flex-1 flex-col gap-2">
+            <label htmlFor="service">Service:</label>
+            <select
+              name="service"
+              id="service"
+              className="rounded-md border border-gray-600/50 bg-black bg-opacity-0 p-2 focus:outline-none"
+            >
+              <option value="usps priority 0-70lbs">
+                USPS Priority 0-70lbs
+              </option>
+            </select>
+          </div>
+          <div className="flex flex-1 flex-col gap-2">
+            <label htmlFor="label">Label:</label>
+            <select
+              name="label"
+              id="label"
+              className="rounded-md border border-gray-600/50 bg-black bg-opacity-0 p-2 focus:outline-none"
+            >
+              <option value="usps priority 0-70lbs">e-VS</option>
+            </select>
+          </div>
 
-            <div className="field-label flex items-end justify-between py-4">
-              Upload a valid CSV to create a label group.
-            </div>
-            <div className="flex items-baseline justify-center gap-4 py-4">
-              <label htmlFor="upload_csv" className="btn-primary">
-                Choose a CSV
-              </label>
-              <div className="paragraph">{fileName}</div>
-              <input
-                onChange={csvHandlingHelper}
-                id="upload_csv"
-                type="file"
-                accept=".csv"
-                className="hidden"
-              />
-            </div>
+          <div className="flex justify-between">
+            <label
+              htmlFor="upload_csv"
+              className="w-40 cursor-pointer items-start rounded-md bg-[#b4a3d8] p-4 text-center text-black"
+            >
+              Choose a CSV
+            </label>
+            <input
+              onChange={csvHandlingHelper}
+              id="upload_csv"
+              type="file"
+              accept=".csv"
+              className="hidden"
+            />
+            <button
+              disabled={true}
+              className="w-40 cursor-pointer items-start rounded-md bg-purple p-4 text-center opacity-50"
+            >
+              Purchase $0
+            </button>
           </div>
         </div>
         {/* <TestLabelsApiReq payload={ payload }/> */}
