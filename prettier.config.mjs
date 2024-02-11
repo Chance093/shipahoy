31e1954225a36776ce49d773b722fd17e7e4d6e4
Chance093 <chance.dev093@gmail.com>
2024-02-11 12:59:23 -0800

    Update prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
-};
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 150,
+  semi: true,
+}
 
-export default config;
+export default config
