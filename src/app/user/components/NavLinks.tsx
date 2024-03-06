2470f6ca96aec5dab12a6db73ffbe675d5c10ced
Chance093 <chance.dev093@gmail.com>
2024-03-05 20:55:43 -0800

    Redirect to landing page after sign out

--- src/app/user/components/NavLinks.tsx
+++ src/app/user/components/NavLinks.tsx
@@ -3,11 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
-import {
-  HomeIcon,
-  CurrencyDollarIcon,
-  DocumentArrowUpIcon,
-} from "@heroicons/react/24/solid";
+import { HomeIcon, CurrencyDollarIcon, DocumentArrowUpIcon } from "@heroicons/react/24/solid";
 import { UserButton, useUser } from "@clerk/nextjs";
 
 const links = [
@@ -34,19 +30,16 @@ export default function Sidebar() {
 
   return (
     <>
-      <nav className="text-custom-gray mx-4 flex flex-col gap-1 border-b border-gray-600/50  py-8 text-lg">
+      <nav className="mx-4 flex flex-col gap-1 border-b border-gray-600/50 py-8  text-lg text-custom-gray">
         {links.map((link) => {
           const LinkIcon = link.icon;
           return (
             <Link
               href={link.href}
               key={link.name}
-              className={clsx(
-                "hover:bg-purple hover:text-custom-white group flex cursor-pointer items-center gap-4 rounded-xl p-3",
-                {
-                  "bg-purple text-custom-white": pathname === link.href,
-                },
-              )}
+              className={clsx("group flex cursor-pointer items-center gap-4 rounded-xl p-3 hover:bg-purple hover:text-custom-white", {
+                "bg-purple text-custom-white": pathname === link.href,
+              })}
             >
               <LinkIcon className="w-6" />
               <p>{link.name}</p>
@@ -55,7 +48,7 @@ export default function Sidebar() {
         })}
       </nav>
       <div className="mx-6 flex flex-1 items-center gap-4 py-8">
-        <UserButton />
+        <UserButton afterSignOutUrl="/" />
         <p>{user ? user.fullName : ""}</p>
       </div>
     </>
