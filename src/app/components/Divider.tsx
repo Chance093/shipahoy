67f989d78235c591825ba4ea37f94d4a92d8e2b9
Chance093 <chance.dev093@gmail.com>
2024-03-06 22:32:25 -0800

    Add Divider component

---
+++ src/app/components/Divider.tsx
@@ -0,0 +1,9 @@
+export default function Divider() {
+  return (
+    <section className="my-8 flex items-center justify-center gap-12">
+      <div className="h-2 w-2 rounded-full bg-white/50"></div>
+      <div className="h-2 w-2 rounded-full bg-white/50"></div>
+      <div className="h-2 w-2 rounded-full bg-white/50"></div>
+    </section>
+  );
+}
--- src/app/user/create-labels/page.tsx
+++ src/app/user/create-labels/page.tsx
@@ -1,18 +1,12 @@
-"use strict";
-// import TestLabelsApiReq from './components/ApiReq';
-
 import HandleCsv from "./components/HandleCsv";
 import SingleLabelCreation from "./components/SingleLabelCreation";
+import Divider from "~/app/components/Divider";
 
-export default function placeholder() {
+export default function CreateLabels() {
   return (
     <>
       <HandleCsv />
-      <section className="my-8 flex items-center justify-center gap-12">
-        <div className="h-2 w-2 rounded-full bg-white/50"></div>
-        <div className="h-2 w-2 rounded-full bg-white/50"></div>
-        <div className="h-2 w-2 rounded-full bg-white/50"></div>
-      </section>
+      <Divider />
       <SingleLabelCreation />
     </>
   );
