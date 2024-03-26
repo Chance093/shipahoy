880cc3962f3f5de112eb203b53e0df0c2ba05d20
proglokan <92769441+proglokan@users.noreply.github.com>
2024-03-25 20:34:43 -0700

    Refactor code to use TypeScript in webhook route and remove unused imports in Modal component

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,46 +1,44 @@
-'use client';
-import { useSearchParams, } from "next/navigation";
-import { useRef, useEffect } from "react";
+"use client";
+import { useRef } from "react";
 
 interface ModalProps {
-    showModal: boolean;
-    title: string;
-    onClose: () => void;
-    children: React.ReactNode;
+  showModal: boolean;
+  title: string;
+  onClose: () => void;
+  children: React.ReactNode;
 }
 
 export default function Modal({ showModal, title, onClose, children }: ModalProps) {
-    // const searchParams = useSearchParams();
-    const modalRef = useRef<HTMLDialogElement | null>(null);
-    if (showModal) modalRef.current?.showModal();
-    // const showModal = searchParams.get('showModal');
+  // const searchParams = useSearchParams();
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+  if (showModal) modalRef.current?.showModal();
+  // const showModal = searchParams.get('showModal');
 
-    // useEffect(() => {
-    //     if (showModal === 'true') modalRef.current?.showModal();
-    //     else modalRef.current?.close();
-    // }, [showModal])
+  // useEffect(() => {
+  //     if (showModal === 'true') modalRef.current?.showModal();
+  //     else modalRef.current?.close();
+  // }, [showModal])
 
-    const closeModal = () => {
-        modalRef.current?.close();
-        onClose();
-    }
+  const closeModal = () => {
+    modalRef.current?.close();
+    onClose();
+  };
 
+  const modal: JSX.Element | null = true ? (
+    <dialog ref={modalRef} className="border-none bg-transparent outline-none backdrop:bg-gray-800/50">
+      <div className="flex items-center justify-center rounded-lg bg-gradient-to-b from-warning">
+        <div className="before-thing flex flex-col rounded-lg bg-[#1a1a1b]">
+          <div className="subheading flex justify-between">
+            <h2 className="ml-4 mt-4">{title}</h2>
+            <button onClick={closeModal} className="mr-4 mt-4 border-none outline-none">
+              ✕
+            </button>
+          </div>
+          <div className="paragraph m-4">{children}</div>
+        </div>
+      </div>
+    </dialog>
+  ) : null;
 
-    const modal: JSX.Element | null = true ? (
-        <dialog ref={ modalRef } className="bg-transparent backdrop:bg-gray-800/50 border-none outline-none">
-            <div className="flex justify-center items-center bg-gradient-to-b from-warning rounded-lg">
-                <div className="flex flex-col bg-[#1a1a1b] rounded-lg before-thing">
-                    <div className="subheading flex justify-between">
-                        <h2 className="mt-4 ml-4">{ title }</h2>
-                        <button onClick={ closeModal } className="mt-4 mr-4 border-none outline-none">✕</button>
-                    </div>
-                    <div className="paragraph m-4">
-                        { children }
-                    </div>
-                </div>
-            </div>
-        </dialog>
-    ) : null;
-
-    return modal;
+  return modal;
 }
