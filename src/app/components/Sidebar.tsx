475041c6eb53e108dbff74377369bb8514254931
Chance093 <chance.dev093@gmail.com>
2024-04-01 21:20:07 -0700

    Add home link to sidebar header

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,13 +1,16 @@
 import NavLinks from "./NavLinks";
 import { GlobeAsiaAustraliaIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 export default function Sidebar() {
   return (
     <aside className="fixed left-0 top-0 h-screen w-72 bg-secondary">
-      <h1 className="mx-4 flex items-center gap-2 border-b border-gray-600/50 py-4 text-2xl">
-        <GlobeAsiaAustraliaIcon className="w-12" />
-        Proglo Shipping
-      </h1>
+      <Link href="/user/dashboard">
+        <h1 className="mx-4 flex items-center gap-2 border-b border-gray-600/50 py-4 text-2xl">
+          <GlobeAsiaAustraliaIcon className="w-12" />
+          Proglo Shipping
+        </h1>
+      </Link>
       <NavLinks />
     </aside>
   );
