16403855bb85be21b75bfa4db05c1dd78f7bc064
proglokan <92769441+proglokan@users.noreply.github.com>
2024-04-19 20:09:47 -0700

    Create initial UI for pricing table

---
+++ src/app/components/PricingTable.tsx
@@ -0,0 +1,41 @@
+export default function PricingTable(userId: string) {
+  return (
+    <div>
+      <div>
+        <label htmlFor="">0-3.99</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">4-7.99</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">8-14.99</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">15-24.99</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">25-34.99</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">35-44.99</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">45-54.99</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">55-64.99</label>
+      </div>
+      <div>
+        <label htmlFor="">65-70</label>
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
