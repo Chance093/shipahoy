f94dc6035b1cfa2fa694cd39b03c3d44aa7cb450
proglokan <92769441+proglokan@users.noreply.github.com>
2024-04-19 20:18:10 -0700

    Create getPricingByUserId query

--- src/server/api/routers/pricing.ts
+++ src/server/api/routers/pricing.ts
@@ -1,7 +1,24 @@
+import { z } from 'zod';
 import { createTRPCRouter, protectedProcedure } from '../trpc';
+import { pricing } from '~/server/db/schema';
+import { eq } from 'drizzle-orm';
+
 
 export const pricingRouter = createTRPCRouter({
-  getUspsPricing: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.query.uspsService.findMany();
-  }),
+  getPricingByUserId: protectedProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.db.query.pricing.findFirst({
+      where: eq(pricing.userId, input),
+      columns: {
+        zeroToFour: true,
+        fourToEight: true,
+        eightToFifteen: true,
+        fifteenToTwentyFive: true,
+        twentyFiveToThirtyFive: true,
+        thirtyFiveToFortyFive: true,
+        fortyFiveToFiftyFive: true,
+        fiftyFiveToSixtyFive: true,
+        sixtyFiveToSeventy: true,
+      },
+    })
+  })
 });
