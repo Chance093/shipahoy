f41ed91fdd5568a4901b3f2999a4b95b6f272072
proglokan <92769441+proglokan@users.noreply.github.com>
2024-04-19 21:19:16 -0700

    Add pricing mutation to useAdmin hook and PricingTable

--- src/app/components/PricingTable.tsx
+++ src/app/components/PricingTable.tsx
@@ -1,41 +1,54 @@
-export default function PricingTable(userId: string) {
+"use client";
+import useAdmin from "~/hooks/useAdmin";
+
+export default function PricingTable() {
+  const { pricingData, setPricingData, updatePricingData } = useAdmin();
+
+  const handleChange = (fields: Partial<typeof pricingData>) => {
+    setPricingData((prev) => {
+      return { ...prev, ...fields };
+    });
+  };
+
   return (
     <div>
       <div>
         <label htmlFor="">0-3.99</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ zeroToFour: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">4-7.99</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ fourToEight: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">8-14.99</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ eightToFifteen: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">15-24.99</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ fifteenToTwentyFive: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">25-34.99</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ twentyFiveToThirtyFive: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">35-44.99</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ thirtyFiveToFortyFive: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">45-54.99</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ fortyFiveToFiftyFive: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">55-64.99</label>
+        <input type="text" onChange={(e) => handleChange({ fiftyFiveToSixtyFive: e.target.value })} />
       </div>
       <div>
         <label htmlFor="">65-70</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange({ sixtyFiveToSeventy: e.target.value })} />
       </div>
+      <button onClick={updatePricingData}>Update Pricing</button>
     </div>
   );
 }
--- src/hooks/useAdmin.tsx
+++ src/hooks/useAdmin.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { api } from "~/trpc/react";
 import { useOrganizationList } from "@clerk/nextjs";
+import { initialPricingState } from "~/lib/lists";
 
 export default function useAdmin() {
   const { userMemberships, isLoaded } = useOrganizationList({ userMemberships: true });
@@ -12,6 +13,8 @@ export default function useAdmin() {
   const { data: orders, refetch: refetchOrders } = api.shippingHistory.getShippingHistoryByUserId.useQuery(userId, { enabled: false });
   const { data: invoices, refetch: refetchInvoices } = api.invoice.getInvoicesByUserId.useQuery(userId, { enabled: false });
   const { data: amount, refetch: refetchBalance } = api.balance.getAmountByUserId.useQuery(userId, { enabled: false });
+  const { data: pricing, refetch: refetchPricing } = api.pricing.getPricingByUserId.useQuery(userId, { enabled: false });
+  const [pricingData, setPricingData] = useState(pricing ?? initialPricingState);
   const updateBalance = api.balance.updateByUserId.useMutation({
     onSuccess: async () => {
       setAddedBalance("");
@@ -25,10 +28,17 @@ export default function useAdmin() {
     },
   });
 
+  const updatePricing = api.pricing.update.useMutation({
+    onSuccess: async () => {
+      await refetchPricing();
+    },
+  });
+
   async function fetchUser() {
     await refetchOrders();
     await refetchInvoices();
     await refetchBalance();
+    await refetchPricing();
   }
 
   function addBalance() {
@@ -38,6 +48,21 @@ export default function useAdmin() {
     updateBalance.mutate({ amount: (Number(amount.amount) + Number(addedBalance)).toString(), userId: userId });
     updateInvoice.mutate({ userId: userId, balanceId: Number(amount.id), amount: addedBalance, paymentMethod: paymentMethod, paymentStatusId: 1 });
   }
+
+  const updatePricingData = () => {
+    if (pricingData === undefined) return;
+    updatePricing.mutate({
+      zeroToFour: pricingData.zeroToFour,
+      fourToEight: pricingData.fourToEight,
+      eightToFifteen: pricingData.eightToFifteen,
+      fifteenToTwentyFive: pricingData.fifteenToTwentyFive,
+      twentyFiveToThirtyFive: pricingData.twentyFiveToThirtyFive,
+      thirtyFiveToFortyFive: pricingData.thirtyFiveToFortyFive,
+      fortyFiveToFiftyFive: pricingData.fortyFiveToFiftyFive,
+      fiftyFiveToSixtyFive: pricingData.fiftyFiveToSixtyFive,
+      sixtyFiveToSeventy: pricingData.sixtyFiveToSeventy,
+    });
+  };
 
   return {
     userMemberships,
@@ -53,5 +78,8 @@ export default function useAdmin() {
     setAddedBalance,
     paymentMethod,
     setPaymentMethod,
+    updatePricingData,
+    pricingData,
+    setPricingData,
   };
 }
