4f03ec24f7b8e7ee864e85a863f38d094e3220bf
Chance093 <chance.dev093@gmail.com>
2024-05-17 21:41:59 -0700

    feat(duoplane): add duoplaneData fetch function

--- src/app/user/duoplane/page.tsx
+++ src/app/user/duoplane/page.tsx
@@ -1,6 +1,9 @@
 import DuoplaneTable from "~/app/components/DuoplaneTable";
+import { fetchDuoplaneData } from "~/lib/duoplane";
 
-export default function Duoplane() {
+export default async function Duoplane() {
+  const duoplaneData = await fetchDuoplaneData();
+
   return (
     <section className="flex flex-1 flex-col rounded-2xl bg-linear-gradient">
       <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col gap-2 rounded-2xl bg-radial-gradient p-5">
