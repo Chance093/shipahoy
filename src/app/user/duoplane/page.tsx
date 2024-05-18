5b70e8fc28abf5a2277eafd2e1f70f2885928fff
Chance093 <chance.dev093@gmail.com>
2024-05-17 20:58:34 -0700

    feat(duoplane): add initial page for duoplane

---
+++ src/app/user/duoplane/page.tsx
@@ -0,0 +1,10 @@
+export default function Duoplane() {
+  return (
+    <section className="flex flex-1 flex-col rounded-2xl bg-linear-gradient">
+      <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col gap-2 rounded-2xl bg-radial-gradient p-5">
+        <h2 className="p-2 text-2xl">Duoplane Orders</h2>
+        <div>Orders</div>
+      </div>
+    </section>
+  );
+}
