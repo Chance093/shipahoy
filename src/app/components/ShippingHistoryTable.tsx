60fe55e9272579742e5faabb414d1cc91141eed2
Chance093 <chance.dev093@gmail.com>
2024-05-25 01:49:08 -0700

    refactor(hooks): create fetchShippingHistory hook
    Create useFetchShippingHistory hook. Extract useQuery from shipping
    table and put into hook. Hook checks if the hook is being called from
    user or admin page and calls the proper procedure.

--- src/app/components/ShippingHistoryTable.tsx
+++ src/app/components/ShippingHistoryTable.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { api } from "~/trpc/react";
 import Orders from "./Orders";
 import Pagination from "./Pagination";
 import TableLoadingSkeleton from "./TableLoadingSkeleton";
 import usePagination from "~/hooks/usePagination";
+import useFetchShippingHistory from "~/hooks/useFetchShippingHistory";
 
 export default function ShippingHistoryTable({
   type,
@@ -15,52 +15,20 @@ export default function ShippingHistoryTable({
   orderCount: number;
 }) {
   const { page, totalPages, incrementPage, decrementPage } = usePagination(orderCount);
+  const { shippingHistory, isShippingHistoryLoading } = useFetchShippingHistory(type, page, userId)!;
 
-  if (type === "user") {
-    const {
-      data: shippingHistory,
-      isError: isShippingHistoryError,
-      error: shippingHistoryError,
-      isLoading: isShippingHistoryLoading,
-    } = api.shippingHistory.getShippingHistory.useQuery(page);
-    if (isShippingHistoryLoading) return <TableLoadingSkeleton title="Shipping History" />;
-    if (isShippingHistoryError) throw shippingHistoryError;
-    if (shippingHistory === undefined) throw new Error("Could not find shipping history");
+  if (isShippingHistoryLoading) return <TableLoadingSkeleton title="Shipping History" />;
+  if (shippingHistory === undefined) throw new Error("Could not fetch shipping history");
 
-    return (
-      <section className="flex flex-1 flex-col rounded-2xl bg-linear-gradient">
-        <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col gap-2 rounded-2xl bg-radial-gradient p-5">
-          <h2 className="p-2 text-2xl">Shipping History</h2>
+  return (
+    <section className="flex flex-1 flex-col rounded-2xl bg-linear-gradient">
+      <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col gap-2 rounded-2xl bg-radial-gradient p-5">
+        <h2 className="p-2 text-2xl">Shipping History</h2>
 
-          <Orders shippingHistory={shippingHistory} page={page} orderCount={orderCount} />
-          <div className="flex-1"></div>
-          <Pagination page={page} decrementPage={decrementPage} incrementPage={incrementPage} totalPages={totalPages} />
-        </div>
-      </section>
-    );
-  }
-
-  if (type === "admin") {
-    if (!userId) return;
-    const {
-      data: shippingHistory,
-      isError: isShippingHistoryError,
-      error: shippingHistoryError,
-      isLoading: isShippingHistoryLoading,
-    } = api.shippingHistory.getShippingHistoryByUserId.useQuery({ page, userId });
-    if (isShippingHistoryLoading) return <TableLoadingSkeleton title="Shipping History" />;
-    if (isShippingHistoryError) throw shippingHistoryError;
-    if (shippingHistory === undefined) throw new Error("Could not find shipping history");
-
-    return (
-      <section className="flex flex-1 flex-col rounded-2xl bg-linear-gradient">
-        <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col gap-2 rounded-2xl bg-radial-gradient p-5">
-          <h2 className="p-2 text-2xl">Shipping History</h2>
-          <Orders shippingHistory={shippingHistory} page={page} orderCount={orderCount} />
-          <div className="flex-1"></div>
-          <Pagination page={page} decrementPage={decrementPage} incrementPage={incrementPage} totalPages={totalPages} />
-        </div>
-      </section>
-    );
-  }
+        <Orders shippingHistory={shippingHistory} page={page} orderCount={orderCount} />
+        <div className="flex-1"></div>
+        <Pagination page={page} decrementPage={decrementPage} incrementPage={incrementPage} totalPages={totalPages} />
+      </div>
+    </section>
+  );
 }
