8fcb8c3533e4b153deba1514e8b9da8f25079d25
Chance093 <chance.dev093@gmail.com>
2024-05-27 20:08:01 -0700

    style(address-form): add address parsing UI
    Add pasted address state for simple typing. Also add a styled button
    for parsing full address. Button has no functionality yet.

--- src/app/user/create-labels/components/AddressForm.tsx
+++ src/app/user/create-labels/components/AddressForm.tsx
@@ -1,11 +1,29 @@
 import { formInputs, statesList } from "~/lib/lists";
 import { type FormData, type HandleChange } from "~/lib/definitions";
+import { useState } from "react";
 
 export default function AddressForm({ formData, handleChange, label }: { formData: FormData; handleChange: HandleChange; label: string }) {
+  const [pastedAddress, setPastedAddress] = useState("");
+
   return (
     <section className="rounded-2xl bg-linear-gradient">
       <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] translate-x-[1.5px] translate-y-[1.5px] flex-col justify-between gap-8 rounded-2xl bg-radial-gradient p-5">
         <h2 className="text-center text-2xl">{label}</h2>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="paste-address">Paste Full Address</label>
+          <input
+            type="text"
+            id="paste-address"
+            onChange={(e) => setPastedAddress(e.target.value)}
+            value={pastedAddress}
+            className="rounded-md border border-gray-600/50 bg-black bg-opacity-0 p-2 focus:outline-none"
+            autoComplete="off"
+          />
+        </div>
+        <button className="w-40 cursor-pointer self-start rounded-md bg-[#b4a3d8] p-4 text-center text-black disabled:opacity-50">
+          Parse Address
+        </button>
+
         {formInputs.map((input, idx) => {
           if (label === "From") {
             if (idx > 8) return;
