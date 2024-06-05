b275d27ee9bb7d924ed07e22091dc65f191176d3
Chance093 <chance.dev093@gmail.com>
2024-06-04 22:23:01 -0700

    feat(duoplane): implement init state functionality
    When you click the plus button, it will create a piece of state for
    that specific po. No styling yet and it still is in its very beginning
    stages

--- src/app/components/DuoplaneTable.tsx
+++ src/app/components/DuoplaneTable.tsx
@@ -1,8 +1,24 @@
 "use client";
 
-import { type duoplaneResponseData } from "~/lib/definitions";
+import { useState } from "react";
+import { type Shipments, type duoplaneResponseData } from "~/lib/definitions";
 
 export default function DuoplaneTable({ duoplaneData }: { duoplaneData: duoplaneResponseData }) {
+  const [shipments, setShipments] = useState<Shipments>([]);
+  const handleClick = (id: string, buyer: string, address: string) => {
+    const newPO = {
+      id,
+      buyer,
+      address,
+      partialShipments: [
+        {
+          weight: "",
+          qty: 0,
+        },
+      ],
+    };
+    setShipments((prev) => [...prev, newPO]);
+  };
   return (
     <>
       {duoplaneData.length === 0 ? (
@@ -13,24 +29,35 @@ export default function DuoplaneTable({ duoplaneData }: { duoplaneData: duoplane
         <table className="w-full text-left">
           <thead className="text-custom-gray ">
             <tr className="border-b border-gray-600/50">
+              <th className="p-4 pb-6 font-normal">Add Shipment</th>
               <th className="p-4 pb-6 font-normal">PO ID</th>
               <th className="p-4 pb-6 font-normal">Buyer</th>
               <th className="p-4 pb-6 font-normal">Address</th>
-              <th className="p-4 pb-6 text-center font-normal">Add Shipment</th>
             </tr>
           </thead>
           <tbody>
             {duoplaneData.map((po) => (
-              <tr key={po.public_reference} className="border-b border-gray-600/50">
-                <td className="p-4 py-6">{po.public_reference}</td>
-                <td className="p-4 py-6">
-                  {po.shipping_address.first_name} {po.shipping_address.last_name}
-                </td>
-                <td className="p-4 py-6">{po.shipping_address.address_1}</td>
-                <td className="p-4 py-6 text-center">
-                  <button>+</button>
-                </td>
-              </tr>
+              <>
+                <tr key={po.public_reference} className="border-b border-gray-600/50">
+                  <td className="p-4 py-6">
+                    <button onClick={() => handleClick(po.public_reference, po.shipping_address.first_name, po.shipping_address.address_1)}>+</button>
+                  </td>
+                  <td className="p-4 py-6">{po.public_reference}</td>
+                  <td className="p-4 py-6">
+                    {po.shipping_address.first_name} {po.shipping_address.last_name}
+                  </td>
+                  <td className="p-4 py-6">{po.shipping_address.address_1}</td>
+                </tr>
+                {shipments.map((shipment, idx) => {
+                  if (shipment?.id === po.public_reference)
+                    return (
+                      <tr key={idx} className="flex justify-between">
+                        <p>weight: {shipment?.partialShipments[idx]?.weight}</p>
+                        <p>qty: {shipment?.partialShipments[idx]?.qty}</p>
+                      </tr>
+                    );
+                })}
+              </>
             ))}
           </tbody>
         </table>
