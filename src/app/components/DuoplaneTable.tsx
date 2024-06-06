db10d4a93e8e76ecdc847d224cd70d16de409fa4
Chance093 <chance.dev093@gmail.com>
2024-06-05 23:38:47 -0700

    feat(duoplane): add state to track partials ships
    At the stage its at now, you can click on the dropdown which works with
    no animation. It then pops up an add shipment button. Once the add
    shipment button is clicked, it will add a partial shipment to that
    specific PO. Still needs styling, the inputs aren't bound to state, and
    there are several bugs

--- src/app/components/DuoplaneTable.tsx
+++ src/app/components/DuoplaneTable.tsx
@@ -3,7 +3,9 @@
 import { useState } from "react";
 import { type Shipments, type duoplaneResponseData } from "~/lib/definitions";
 
-export default function DuoplaneTable({ duoplaneData }: { duoplaneData: duoplaneResponseData }) {
+export default function DuoplaneTable({ data }: { data: duoplaneResponseData }) {
+  const duoplaneData = data.map((data) => ({ ...data, active: false }));
+  const [duoplaneState, setDuoplaneState] = useState(duoplaneData);
   const [shipments, setShipments] = useState<Shipments>([]);
   const handleClick = (id: string, buyer: string, address: string) => {
     const newPO = {
@@ -19,9 +21,21 @@ export default function DuoplaneTable({ duoplaneData }: { duoplaneData: duoplane
     };
     setShipments((prev) => [...prev, newPO]);
   };
+
+  const dropDown = (id: string) => {
+    const updatedDuoplaneState = duoplaneState.map((state) => {
+      if (state.public_reference === id) {
+        if (state.active === true) return { ...state, active: false };
+        return { ...state, active: true };
+      }
+      return state;
+    });
+
+    setDuoplaneState(updatedDuoplaneState);
+  };
   return (
     <>
-      {duoplaneData.length === 0 ? (
+      {duoplaneState.length === 0 ? (
         <div className="flex flex-1 items-center justify-center pb-8">
           <p className="text-2xl">You have no orders!</p>
         </div>
@@ -29,18 +43,18 @@ export default function DuoplaneTable({ duoplaneData }: { duoplaneData: duoplane
         <table className="w-full text-left">
           <thead className="text-custom-gray ">
             <tr className="border-b border-gray-600/50">
-              <th className="p-4 pb-6 font-normal">Add Shipment</th>
+              <th className="p-4 pb-6 font-normal"></th>
               <th className="p-4 pb-6 font-normal">PO ID</th>
               <th className="p-4 pb-6 font-normal">Buyer</th>
               <th className="p-4 pb-6 font-normal">Address</th>
             </tr>
           </thead>
           <tbody>
-            {duoplaneData.map((po) => (
+            {duoplaneState.map((po) => (
               <>
                 <tr key={po.public_reference} className="border-b border-gray-600/50">
                   <td className="p-4 py-6">
-                    <button onClick={() => handleClick(po.public_reference, po.shipping_address.first_name, po.shipping_address.address_1)}>+</button>
+                    <button onClick={() => dropDown(po.public_reference)}>v</button>
                   </td>
                   <td className="p-4 py-6">{po.public_reference}</td>
                   <td className="p-4 py-6">
@@ -48,15 +62,32 @@ export default function DuoplaneTable({ duoplaneData }: { duoplaneData: duoplane
                   </td>
                   <td className="p-4 py-6">{po.shipping_address.address_1}</td>
                 </tr>
-                {shipments.map((shipment, idx) => {
-                  if (shipment?.id === po.public_reference)
-                    return (
-                      <tr key={idx} className="flex justify-between">
-                        <p>weight: {shipment?.partialShipments[idx]?.weight}</p>
-                        <p>qty: {shipment?.partialShipments[idx]?.qty}</p>
-                      </tr>
-                    );
-                })}
+                {po.active ? (
+                  <>
+                    {shipments.map((shipment) => {
+                      if (shipment?.id === po.public_reference)
+                        return (
+                          <tr key={shipment.id} className="flex justify-between">
+                            {shipment.partialShipments.map((partialShipment) => (
+                              <>
+                                <div>
+                                  <label htmlFor="">weight</label>
+                                  <input className="text-black" type="text" value={partialShipment.weight} />
+                                </div>
+                                <div>
+                                  <label htmlFor="">qty</label>
+                                  <input className="text-black" type="text" value={partialShipment.qty} />
+                                </div>
+                              </>
+                            ))}
+                          </tr>
+                        );
+                    })}
+                    <button onClick={() => handleClick(po.public_reference, po.shipping_address.first_name, po.shipping_address.address_1)}>
+                      Add Shipment
+                    </button>
+                  </>
+                ) : null}
               </>
             ))}
           </tbody>
--- src/app/user/duoplane/page.tsx
+++ src/app/user/duoplane/page.tsx
@@ -10,7 +10,7 @@ export default async function Duoplane() {
     <section className="flex flex-1 flex-col rounded-2xl bg-linear-gradient">
       <div className="flex h-[calc(100%-3px)] w-[calc(100%-3px)] flex-1 translate-x-[1.5px] translate-y-[1.5px] flex-col gap-2 rounded-2xl bg-radial-gradient p-5">
         <h2 className="p-2 text-2xl">Duoplane Orders</h2>
-        <DuoplaneTable duoplaneData={duoplaneData} />
+        <DuoplaneTable data={duoplaneData} />
       </div>
     </section>
   );
