3635b76acbef2608e364e70b277fc624814d4086
Chance093 <chance.dev093@gmail.com>
2024-07-03 23:04:04 -0700

    build(clerk): rollback clerk version

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware((auth, req) => {
-
-}, { debug: true })
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({
+  publicRoutes: ["/", "/api/webhook", "/api/temp"],
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
